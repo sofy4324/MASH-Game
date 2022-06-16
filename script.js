2aca5137d93f3929d73d239f4b7f0112864dae11
sofy4324 <107209366+sofy4324@users.noreply.github.com>
2022-06-15 19:58:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,17 +32,20 @@ function get_answer(category) {
 // Function to find the spans that need to be filled
 function fill_in_answers(answers) {
 	var home = document.querySelector('#home');  // This says make a new variable and find the HTML tag that has the ID of "home" 
+	var answer_4 = document.querySelector('#answer_4');
 	var answer_2 = document.querySelector('#answer_2');
 	var answer_3 = document.querySelector('#answer_3');
 	var answer_1 = document.querySelector('#answer_1');
 	
 	// Fill them with the provided answers
 	home.innerText = answers['mash'];
+	answer_4.innerText = answers['answer_4'];
 	answer_2.innerText = answers['answer_2'];
 	answer_3.innerText = answers['answer_3'];
 	answer_1.innerText = answers['answer_1'];
 	home.innerHTML = answers.mash;  // Change the content of the element in the HTML doc with the id "home" to the "mash" value in answers 
-	answer_2.innerHTML = answers.answer_2;  // Change the content of the element in the HTML doc with the id "career" to the "career" value in answers 
+	answer_2.innerHTML = answers.answer_4;// Change the content of the element in the HTML doc with the id "career" to the "career" value in answers 
+	answer_2.innerHTML = answers.answer_2; 
 	answer_3.innerHTML = answers.answer_3;
 	answer_1.innerHTML = answers.answer_1;
 }
@@ -55,9 +58,10 @@ function handle_submission(evt) {
 	// Build up our answers object
 	var answers = {
 	    'mash': mash_choice(),
-	    'answer_2': get_answer('answer_2'),
-	    'answer_3': get_answer('answer_3'),
-	    'answer_1': get_answer('answer_1')
+	   'answer_4': get_answer('answer_4'),
+	   'answer_2': get_answer('answer_2'),
+	   'answer_3': get_answer('answer_3'),
+	   'answer_1': get_answer('answer_1')
 	}
 	// Fill in the answers
 	fill_in_answers(answers);
